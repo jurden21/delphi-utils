61358173e208733290c4bd829088b33a3dfac240
jurden <dev@jurden.ru>
2024-06-24 22:32:37 +0300

    Add TIniUtil class and tests

---
+++ src/IniUtilTestsUnit.pas
@@ -0,0 +1,131 @@
+unit IniUtilTestsUnit;
+
+interface
+
+uses
+  DUnitX.TestFramework, System.Classes, System.SysUtils;
+
+const
+  FILE_NAME = 'test.ini';
+
+type
+  [TestFixture]
+  TIniUtilTests = class(TObject)
+  private
+  public
+    [Setup]
+    procedure Setup;
+    [TearDown]
+    procedure TearDown;
+    [Test]
+    procedure ReadStringDefaultFileTest;
+    [Test]
+    procedure ReadStringCustomFileTest;
+    [Test]
+    procedure WriteStringDefaultFileTest;
+    [Test]
+    procedure WriteStringCustomFileTest;
+  end;
+
+implementation
+
+uses
+  IniUtilUnit;
+
+procedure TIniUtilTests.Setup;
+var
+  StringList: TStringList;
+begin
+  StringList := TStringList.Create;
+  StringList.Add('[SECTION1]');
+  StringList.Add('StringValue1=string_value_1');
+  StringList.Add('[SECTION2]');
+  StringList.Add('StringValue2=string_value_2');
+  StringList.SaveToFile(ChangeFileExt(ParamStr(0), '.ini'));
+
+  StringList := TStringList.Create;
+  StringList.Add('[SECTION6]');
+  StringList.Add('StringValue6=string_value_6');
+  StringList.Add('[SECTION7]');
+  StringList.Add('StringValue7=string_value_7');
+  StringList.SaveToFile(IncludeTrailingPathDelimiter(ExtractFilePath(ParamStr(0))) + FILE_NAME);
+end;
+
+procedure TIniUtilTests.TearDown;
+begin
+  DeleteFile(ChangeFileExt(ParamStr(0), '.ini'));
+  DeleteFile(IncludeTrailingPathDelimiter(ExtractFilePath(ParamStr(0))) + FILE_NAME);
+end;
+
+procedure TIniUtilTests.ReadStringDefaultFileTest;
+var
+  ExpectedValue, ActualValue: String;
+begin
+  ExpectedValue := 'string_value_1';
+  ActualValue := TIniUtil.ReadString('SECTION1', 'StringValue1', 'DefaultStringValue1');
+  Assert.AreEqual(ExpectedValue, ActualValue);
+
+  ExpectedValue := 'string_value_2';
+  ActualValue := TIniUtil.ReadString('SECTION2', 'StringValue2', 'DefaultStringValue2');
+  Assert.AreEqual(ExpectedValue, ActualValue);
+
+  ExpectedValue := 'DefaultStringValue3';
+  ActualValue := TIniUtil.ReadString('SECTION3', 'StringValue3', 'DefaultStringValue3');
+  Assert.AreEqual(ExpectedValue, ActualValue);
+end;
+
+procedure TIniUtilTests.ReadStringCustomFileTest;
+var
+  ExpectedValue, ActualValue: String;
+begin
+  TIniUtil.FileName := IncludeTrailingPathDelimiter(ExtractFilePath(ParamStr(0))) + FILE_NAME;
+
+  ExpectedValue := 'string_value_6';
+  ActualValue := TIniUtil.ReadString('SECTION6', 'StringValue6', 'DefaultStringValue6');
+  Assert.AreEqual(ExpectedValue, ActualValue);
+
+  ExpectedValue := 'string_value_7';
+  ActualValue := TIniUtil.ReadString('SECTION7', 'StringValue7', 'DefaultStringValue7');
+  Assert.AreEqual(ExpectedValue, ActualValue);
+
+  ExpectedValue := 'DefaultStringValue8';
+  ActualValue := TIniUtil.ReadString('SECTION8', 'StringValue8', 'DefaultStringValue8');
+  Assert.AreEqual(ExpectedValue, ActualValue);
+end;
+
+procedure TIniUtilTests.WriteStringDefaultFileTest;
+var
+  ExpectedValue, ActualValue: String;
+begin
+  ExpectedValue := 'string_value_3';
+  TIniUtil.WriteString('SECTION3', 'StringValue3', ExpectedValue);
+  ActualValue := TIniUtil.ReadString('SECTION3', 'StringValue3', 'DefaultStringValue3');
+  Assert.AreEqual(ExpectedValue, ActualValue);
+
+  ExpectedValue := 'string_value_3';
+  TIniUtil.WriteString('SECTION2', 'StringValue3', ExpectedValue);
+  ActualValue := TIniUtil.ReadString('SECTION2', 'StringValue3', 'DefaultStringValue3');
+  Assert.AreEqual(ExpectedValue, ActualValue);
+end;
+
+procedure TIniUtilTests.WriteStringCustomFileTest;
+var
+  ExpectedValue, ActualValue: String;
+begin
+  TIniUtil.FileName := IncludeTrailingPathDelimiter(ExtractFilePath(ParamStr(0))) + FILE_NAME;
+
+  ExpectedValue := 'string_value_3';
+  TIniUtil.WriteString('SECTION3', 'StringValue3', ExpectedValue);
+  ActualValue := TIniUtil.ReadString('SECTION3', 'StringValue3', 'DefaultStringValue3');
+  Assert.AreEqual(ExpectedValue, ActualValue);
+
+  ExpectedValue := 'string_value_3';
+  TIniUtil.WriteString('SECTION2', 'StringValue3', ExpectedValue);
+  ActualValue := TIniUtil.ReadString('SECTION2', 'StringValue3', 'DefaultStringValue3');
+  Assert.AreEqual(ExpectedValue, ActualValue);
+end;
+
+initialization
+  TDUnitX.RegisterTestFixture(TIniUtilTests);
+
+end.
